96d1e23eacbb56dabb60a683dbcdbab7283e49ba
Nav <nav123@gmail.com>
2023-11-21 10:42:00 -0800

    Added some database values to distinguish between users

--- database.js
+++ database.js
@@ -12,12 +12,14 @@ const database = [
     name: "Johnny Doe",
     email: "johnny123@gmail.com",   
     password: "johnny123!",
+    reminders: [{title: "Buy groceries", description: "Buy milk, eggs, and bread", completed: false}],
   },
   {
     id: 3,
     name: "Jonathan Chen",
     email: "jonathan123@gmail.com",
     password: "jonathan123!",
+    reminders: [{title: "Play football", description: "Play 30 mins a day ", completed: false}]
   },
 ];
 
